4902f207f4fdf8fd535a9b60d33a3c68dcf8e524
Dejan Fajfar <dejan@fajfar.com>
2014-01-16 22:04:19 +0100

    Adapted test file to the MenuManager move

--- lib/Test.rb
+++ lib/Test.rb
@@ -1,6 +1,6 @@
 require 'colorize'
-require_relative 'menu_manager'
+require_relative 'ui/menu_manager'
 require 'yaml'
 
-menu_printer = MenuManager.new
+menu_printer = Ui::MenuManager.new
 menu_printer.display_menu "MainMenu"
